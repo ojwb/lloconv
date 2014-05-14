955d8a6d6c6fe4071a81736ed13cd25f60096bdd
Olly Betts <olly@survex.com>
2014-05-14 13:05:59 +1200

    Add -f option to allow explicitly specifying output format

--- lloconv.cc
+++ lloconv.cc
@@ -18,11 +18,37 @@ using namespace std;
 
 #define LO_PATH "/opt/libreoffice4.2/program"
 
+static const char * program = "lloconv";
+
+static void
+usage()
+{
+    cerr << "Usage: " << program << " [-f FORMAT] INPUT_FILE OUTPUT_FILE" << endl;
+}
+
 int
 main(int argc, char **argv)
 try {
+    program = argv[0];
+
+    if (argc < 3) {
+	usage();
+	_Exit(1);
+    }
+
+    const char * format = NULL;
+    if (argv[1][0] == '-' && argv[1][1] == 'f') {
+	if (argv[1][2]) {
+	    format = argv[1] + 2;
+	    ++argv;
+	} else {
+	    format = argv[2];
+	    argv += 2;
+	}
+    }
+
     if (argc != 3) {
-        cerr << "Usage: " << argv[0] << " INPUT_FILE OUTPUT_FILE" << endl;
+	usage();
 	_Exit(1);
     }
 
@@ -35,26 +61,26 @@ try {
     }
     LibLibreOffice * llo = lo_cpp_init(lo_path);
     if (!llo || !llo->initialize(lo_path)) {
-        cerr << argv[0] << ": Failed to initialise liblibreoffice" << endl;
+        cerr << program << ": Failed to initialise liblibreoffice" << endl;
 	_Exit(1);
     }
 
     LODocument * lodoc = llo->documentLoad(input);
     if (!lodoc) {
 	const char * errmsg = llo->getError();
-        cerr << argv[0] << ": liblibreoffice failed to load document (" << errmsg << ")" << endl;
+        cerr << program << ": liblibreoffice failed to load document (" << errmsg << ")" << endl;
 	_Exit(1);
     }
 
-    if (!lodoc->saveAs(output, NULL)) {
+    if (!lodoc->saveAs(output, format)) {
 	const char * errmsg = llo->getError();
-        cerr << argv[0] << ": liblibreoffice failed to export (" << errmsg << ")" << endl;
+        cerr << program << ": liblibreoffice failed to export (" << errmsg << ")" << endl;
 	_Exit(1);
     }
 
     // Avoid segfault from libreoffice by terminating swiftly.
     _Exit(0);
 } catch (const exception & e) {
-    cerr << argv[0] << ": liblibreoffice threw exception (" << e.what() << ")" << endl;
+    cerr << program << ": liblibreoffice threw exception (" << e.what() << ")" << endl;
     _Exit(1);
 }
