14b131fa565f2e48895c26362c13cc76a553972f
Olly Betts <olly@survex.com>
2016-04-29 11:59:36 +1200

    Check for upstream LO 5.1 build

--- convert.cc
+++ convert.cc
@@ -41,6 +41,7 @@ get_lo_path()
 	struct stat sb;
 #define CHECK_DIR(P) if (!lo_path && stat(P"/versionrc", &sb) == 0 && S_ISREG(sb.st_mode)) lo_path = P
 	CHECK_DIR(LO_PATH_DEBIAN);
+	CHECK_DIR(LO_PATH_LIBREOFFICEORG(5.1));
 	CHECK_DIR(LO_PATH_LIBREOFFICEORG(5.0));
 	CHECK_DIR(LO_PATH_LIBREOFFICEORG(4.4));
 	CHECK_DIR(LO_PATH_LIBREOFFICEORG(4.3));
